24d8bf05c9ebd2948018c578a798888fb7d7cb8a
twolak88 <twolak88@gmail.com>
2021-02-21 09:35:09 +0100

    initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { ReversePipe } from './shared/reverse.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ReversePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../shared/data.service';
+import { UserService } from './user.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  user: {name: string};
+  isLoggedIn = false;
+  data: string;
+
+  constructor(private userService: UserService,
+    private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.user = this.userService.user;
+    this.dataService.getDetails().then((data: string) => this.data = data);
+  }
+
+}
