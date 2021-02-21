79802bdb266c32a366622c8ce765d9a7909348c2
twolak88 <twolak88@gmail.com>
2021-02-21 12:43:06 +0100

    Testing Dependencies: Components and Services

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { UserService } from './user.service';
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -16,10 +17,29 @@ describe('UserComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the app', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should use user name from service', () => {
+    fixture.detectChanges(); //important!
+    let userService = fixture.debugElement.injector.get(UserService);
+    expect(userService.user.name).toEqual(component.user.name);
+  });
+
+  it('should display the user name if user is logged in', () => {
+    component.isLoggedIn = true;
+    fixture.detectChanges(); //important!
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain(component.user.name);
+  });
+
+  it('shouldn\'t display the user name if user is logged in', () => {
+    fixture.detectChanges(); //important!
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).not.toContain(component.user.name);
+    expect(compiled.querySelector('p').textContent).toContain('Please log in first');
   });
 });
